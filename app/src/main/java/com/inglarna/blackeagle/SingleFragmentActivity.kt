9875069b0f69457820387fc79175d125bd9f7515
melvin <melvin.larsson@hotmail.se>
2021-10-31 19:20:22 +0100

    Implemented SingleFragmentActivity

---
+++ app/src/main/java/com/inglarna/blackeagle/SingleFragmentActivity.kt
@@ -0,0 +1,20 @@
+package com.inglarna.blackeagle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+abstract class SingleFragmentActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, createFragment())
+                .commitNow()
+        }
+    }
+    abstract fun createFragment() : Fragment
+
+}
