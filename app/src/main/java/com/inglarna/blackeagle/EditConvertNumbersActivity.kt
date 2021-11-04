67b151ae7aa3ebda14caa9e21238a0c1cc2740dc
runne <runneberg@live.se>
2021-11-04 17:14:29 +0100

    added backbutton

--- app/src/main/java/com/inglarna/blackeagle/EditConvertNumbersActivity.kt
+++ app/src/main/java/com/inglarna/blackeagle/EditConvertNumbersActivity.kt
@@ -1,11 +1,13 @@
 package com.inglarna.blackeagle
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.inglarna.blackeagle.ui.EditConvertNumbersFragment
 
-class EditConvertNumbersActivity: SingleFragmentActivity() {
+class EditConvertNumbersActivity: HamburgermenuActivity() {
     override fun createFragment(): Fragment {
         val fragment = EditConvertNumbersFragment.newInstance()
         return fragment
     }
+
 }
