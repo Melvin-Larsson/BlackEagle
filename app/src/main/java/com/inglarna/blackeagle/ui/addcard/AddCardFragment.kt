7b05503a0e2dcad2582c5fc7bc2c542737afd0b4
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2021-11-23 20:06:27 +0100

    Stored AddCard input fields in card object

--- app/src/main/java/com/inglarna/blackeagle/ui/addcard/AddCardFragment.kt
+++ app/src/main/java/com/inglarna/blackeagle/ui/addcard/AddCardFragment.kt
@@ -8,9 +8,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.inglarna.blackeagle.databinding.FragmentEditCardBinding
 import com.inglarna.blackeagle.model.Card
+import com.inglarna.blackeagle.repository.DeckRepo
 
 class AddCardFragment : Fragment() {
     lateinit var binding : FragmentEditCardBinding
+    lateinit var deckRepo: DeckRepo
 
     companion object{
         private const val DECK_ID = "deckID"
@@ -22,18 +24,18 @@ class AddCardFragment : Fragment() {
             return addCardFragment
         }
     }
-
-
     override fun onCreateView(inflater: LayoutInflater,container: ViewGroup?,savedInstanceState: Bundle?): View? {
         binding = FragmentEditCardBinding.inflate(inflater, container, false)
-
-
+        deckRepo = DeckRepo(requireContext())
         binding.buttonAddCard.setOnClickListener{
-            var question = binding.editTextQuestion.text
-            var answer = binding.editTextAnswer.text
-            var hint = binding.hint.text
+            var question = binding.editTextQuestion.text.toString()
+            var answer = binding.editTextAnswer.text.toString()
+            var hint = binding.hint.text.toString()
 
-            var temp = Card()
+            var card = Card()
+            card.question = question
+            card.answer = answer
+            card.hint = hint
         }
         return binding.root
     }
