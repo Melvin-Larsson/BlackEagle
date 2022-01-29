e34a311416bfd7ae01503eaa8ee443c528d09814
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2022-01-29 15:26:16 +0100

    Implemented dark mode switch

--- app/src/main/java/com/inglarna/blackeagle/ui/SingleFragmentActivity.kt
+++ app/src/main/java/com/inglarna/blackeagle/ui/SingleFragmentActivity.kt
@@ -1,8 +1,11 @@
 package com.inglarna.blackeagle.ui
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
 import com.inglarna.blackeagle.R
 import com.inglarna.blackeagle.databinding.ActivityFragmentBinding
 
@@ -12,6 +15,10 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Theme
+        refreshTheme()
+
         //Create view
         binding = ActivityFragmentBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -21,12 +28,22 @@ abstract class SingleFragmentActivity : AppCompatActivity(){
                 .commitNow()
         }
 
+
     }
     abstract fun createFragment(): Fragment
-
 
     override fun finish() {
         super.finish()
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+
+    fun refreshTheme(){
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val darkMode = sharedPreferences.getBoolean("darkMode", false)
+        if(darkMode){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }else{
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
     }
 }
--- app/src/main/java/com/inglarna/blackeagle/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/inglarna/blackeagle/ui/settings/SettingsFragment.kt
@@ -1,47 +1,37 @@
 package com.inglarna.blackeagle.ui.settings
 
-import android.graphics.Color
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import com.github.mikephil.charting.data.BarData
-import com.github.mikephil.charting.data.BarDataSet
-import com.github.mikephil.charting.data.BarEntry
-import com.github.mikephil.charting.utils.ColorTemplate
-import com.inglarna.blackeagle.databinding.FragmentSettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
+import com.inglarna.blackeagle.R
+import com.inglarna.blackeagle.ui.SingleFragmentActivity
 
-class SettingsFragment: Fragment() {
-    lateinit var barList:ArrayList<BarEntry>
-    lateinit var binding : FragmentSettingsBinding
-    lateinit var barDataSet: BarDataSet
-    lateinit var barData: BarData
+class SettingsFragment: PreferenceFragmentCompat(),
+    SharedPreferences.OnSharedPreferenceChangeListener {
 
     companion object {
         fun newInstance() = SettingsFragment()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onResume() {
+        super.onResume()
+        PreferenceManager.getDefaultSharedPreferences(requireContext()).registerOnSharedPreferenceChangeListener(this)
+    }
 
-        barList = ArrayList()
-        barList.add(BarEntry(10f, 100f))
-        barList.add(BarEntry(10f, 100f))
-        barList.add(BarEntry(10f, 100f))
-        barList.add(BarEntry(10f, 100f))
-        barList.add(BarEntry(10f, 100f))
-        barList.add(BarEntry(10f, 100f))
-        barList.add(BarEntry(10f, 100f))
-        barDataSet = BarDataSet(barList, "studies")
-        barData = BarData(barDataSet)
-        barDataSet.setColors(ColorTemplate.JOYFUL_COLORS, 250)
-        barDataSet.valueTextColor = Color.BLACK
-        barDataSet.valueTextSize = 15f
+    override fun onPause() {
+        super.onPause()
+        PreferenceManager.getDefaultSharedPreferences(requireContext()).unregisterOnSharedPreferenceChangeListener(this)
+
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        if(key == "darkMode"){
+            (requireActivity() as SingleFragmentActivity).refreshTheme()
+        }
     }
 }
