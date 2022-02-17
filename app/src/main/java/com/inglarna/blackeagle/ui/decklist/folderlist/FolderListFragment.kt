0c32d734b2732d4f24665e9fe4226fcedd357945
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2022-02-17 17:18:17 +0100

    Fix: navmenu crash in start screen folder list

--- app/src/main/java/com/inglarna/blackeagle/ui/decklist/folderlist/FolderListFragment.kt
+++ app/src/main/java/com/inglarna/blackeagle/ui/decklist/folderlist/FolderListFragment.kt
@@ -1,29 +1,19 @@
 package com.inglarna.blackeagle.ui.decklist.folderlist
 
-import android.app.AlertDialog
 import android.os.Bundle
-import android.text.InputType
 import android.view.*
-import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.inglarna.blackeagle.R
 import com.inglarna.blackeagle.databinding.FragmentFolderListBinding
-import com.inglarna.blackeagle.model.Folder
-import com.inglarna.blackeagle.ui.cardlist.CardListActivity
-import com.inglarna.blackeagle.ui.cardlist.EditDeckActivity
 import com.inglarna.blackeagle.ui.decklist.EditableListActivity
 import com.inglarna.blackeagle.ui.folder.FolderActivity
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class FolderListFragment : Fragment() {
     private lateinit var binding : FragmentFolderListBinding
     private val viewModel by viewModels<FolderListViewModel>()
     private lateinit var adapter: FolderListRecyclerViewAdapter
-    private lateinit var addFolderMenuItem: MenuItem
 
     companion object{
         fun newInstance(): FolderListFragment = FolderListFragment()
@@ -40,17 +30,17 @@ class FolderListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        adapter = FolderListRecyclerViewAdapter(context!!, viewModel, viewLifecycleOwner)
+        adapter = FolderListRecyclerViewAdapter(requireContext(), viewModel, viewLifecycleOwner)
         adapter.onFolderSelected = { folder ->
-            startActivity(FolderActivity.newIntent(context!!, folder.folderId!!))
+            startActivity(FolderActivity.newIntent(requireContext(), folder.folderId!!))
         }
         binding.folderRecyclerView.adapter = adapter
         binding.folderRecyclerView.layoutManager = LinearLayoutManager(requireContext())
 
-        viewModel.folders.observe(this, {
+        viewModel.folders.observe(viewLifecycleOwner){
             adapter.folders = it
-        })
-        viewModel.select.observe(this){select ->
+        }
+        viewModel.select.observe(viewLifecycleOwner){select ->
             (requireActivity() as EditableListActivity).setSelectVisibility(select)
         }
 
@@ -58,10 +48,6 @@ class FolderListFragment : Fragment() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
-            android.R.id.home ->{
-                activity!!.finish()
-                return true
-            }
             R.id.delete -> viewModel.removeSelectedFolders()
             R.id.selectAll -> viewModel.toggleSelectAll()
             R.id.closeSelect -> viewModel.setSelect(false)
