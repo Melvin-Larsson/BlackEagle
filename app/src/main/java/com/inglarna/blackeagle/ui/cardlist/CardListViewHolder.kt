1b93b8ee6c43bafbef3baaf654096f3cf662a89e
Melvin-Larsson <Melvin.Larsson@Hotmail.se>
2022-02-17 17:14:39 +0100

    Removed edit deck name toast

--- app/src/main/java/com/inglarna/blackeagle/ui/cardlist/CardListViewHolder.kt
+++ app/src/main/java/com/inglarna/blackeagle/ui/cardlist/CardListViewHolder.kt
@@ -1,14 +1,7 @@
 package com.inglarna.blackeagle.ui.cardlist
 
-import android.view.View
-import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.inglarna.blackeagle.databinding.ListItemCardBinding
-import android.view.MenuItem
-import androidx.appcompat.widget.PopupMenu
-import com.inglarna.blackeagle.R
-import kotlinx.coroutines.selects.select
-
 
 class CardListViewHolder(val binding: ListItemCardBinding): RecyclerView.ViewHolder(binding.root){
 }
--- app/src/main/java/com/inglarna/blackeagle/ui/cardlist/EditDeckFragment.kt
+++ app/src/main/java/com/inglarna/blackeagle/ui/cardlist/EditDeckFragment.kt
@@ -8,7 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -123,7 +122,6 @@ class EditDeckFragment : Fragment() {
                 GlobalScope.launch {
                     deckDao.updateDeck(deck)
                 }
-                Toast.makeText(requireContext(), "edited $deck", Toast.LENGTH_SHORT).show()
                 dialog.dismiss()
             }
             .create()
